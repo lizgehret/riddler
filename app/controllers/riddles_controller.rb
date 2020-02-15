4ab669d43a7c4a7617a1e0dee379c4bbd2b64c2d
Liz Gehret <elizabeth.gehret@gmail.com>
2020-02-15 22:00:44 +0000

    Adding simple form, and storing riddles in database upon form submission

--- app/controllers/riddles_controller.rb
+++ app/controllers/riddles_controller.rb
@@ -2,4 +2,19 @@ class RiddlesController < ApplicationController
   def index
     @riddle = Riddle.order("RANDOM()").first
   end
+
+  def new
+    @riddle = Riddle.new
+  end
+
+  def create
+    Riddle.create(riddle_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def riddle_params
+    params.require(:riddle).permit(:riddle, :answer, :riddler)
+  end
 end
